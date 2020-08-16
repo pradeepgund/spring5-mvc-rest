f891d74c5b3997a28b264085b168bf0c233f5ad0
pradeepgund <prdpgnd@gmail.com>
2020-08-16 23:27:50 +0530

    Adding Category Service

---
+++ src/main/java/com/pradeep/service/CategoryService.java
@@ -0,0 +1,12 @@
+package com.pradeep.service;
+
+import com.pradeep.api.v1.model.CategoryDTO;
+
+import java.util.List;
+
+public interface CategoryService {
+
+    List<CategoryDTO> getAllCategories();
+
+    CategoryDTO getCategoryByName(String name);
+}
